5ba9ef4ffb16549745100f301928935881eb2408
Tuan0324 <jhmh0226@gmail.com>
2021-01-28 16:55:39 +0900

    20210128-2

--- chapter2/nkc.py
+++ chapter2/nkc.py
@@ -2,10 +2,14 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import requests
-from bs4 import BeautifulSoup
 import re
+import random
+import pytagcloud
+import webbrowser
+from bs4 import BeautifulSoup
 from konlpy.tag import Okt
 from collections import Counter
+from IPython.display import Image
 
 #크롤링할 사이트 주소 정의
 source_url = 'https://namu.wiki/RecentChanges'
@@ -82,17 +86,44 @@ def text_cleaning(text):
     result = hangul.sub('', text)
     return result
 
+# 피처 마다 데이터 전처리 적용
 df['title'] = df['title'].apply(lambda x : text_cleaning(x))
 df['category'] = df['category'].apply(lambda x : text_cleaning(x))
 df['content_text'] = df['content_text'].apply(lambda x : text_cleaning(x))
 
+# 피처 마다 말뭉치 생성
 title_corpus = ''.join(df['title'].tolist())
 catetgory_corpus = ''.join(df['category'].tolist())
 content_corpus = ''.join(df['content_text'].tolist())
 
+#konlpy의 형태소 분석 모듈을 사용하여 키워드 추출
 nouns_tagger = Okt()
 nouns = nouns_tagger.nouns(content_corpus)
 count = Counter(nouns)
 
-print(count)
+# 한글자 키워드 제거
+remove_char_counter = Counter({x : count[x] for x in count if len(x) > 1})
 
+# 불용어 리스트를 오픈합니다.
+korean_stopword_path = '/Users/tuan/Documents/Python/python-data-analysis-master/data/korean_stopwords.txt'
+with open(korean_stopword_path, encoding= 'utf8') as f:
+    stopwords = f.readlines()
+stopwords = [x.strip() for x in stopwords]
+print(stopwords[:10])
+
+#적용이 필요한 불용어를 추가합니다.
+namu_wiki_stopwords = ['상위', '문서', '내용', '누설', '아래', '해당', '표기', '설명', '추가', '모든', '사용', '매우', '가장', '줄거리', '요소', '상황', '편집', '틀', 
+                        '경우', '때문', '모습', '정도', '이후', '사실', '생각', '인물', '이름', '년월']
+for stopword in namu_wiki_stopwords:
+    stopwords.append(stopword)
+
+# 키워드 데이터에서 불용어를 제거 합니다
+remove_char_counter = Counter({x : remove_char_counter[x] for x in count if x not in stopwords})
+
+ranked_tags = remove_char_counter.most_common(40)
+
+taglist = pytagcloud.make_tags(ranked_tags, maxsize = 80)
+
+pytagcloud.create_tag_image(taglist, 'wordcloud.jpg', size = (900, 600), fontname = 'NanumGothic', rectangular = False)
+
+Image(filename = 'wordcloud.jpg')
